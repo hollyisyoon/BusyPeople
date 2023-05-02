f282bd56aab7971b7dd579983283a7c5791975eb
Song-Juntae <123853844+Song-Juntae@users.noreply.github.com>
2023-05-03 02:06:30 +0900

    .

--- pages/👀 트렌드 알아보쟙.py
+++ pages/👀 트렌드 알아보쟙.py
@@ -93,8 +93,7 @@ st.pyplot(fig1)
 # Create bar graph
 words_count = Counter(words)
 words_df = pd.DataFrame([words_count])
-words_df = words_df.sort_values('count', ascending=False, inplace=True)
-words
+# words_df = words_df.sort_values('count', ascending=False, inplace=True)
 
 # fig2, ax2 = plt.subplots(figsize=(10, 4))
 # words_df.plot(kind='bar', ax=ax2)
