0cd3594b61c16b3e952162aeaa1d45f022774a3c
Thinking Seo <110218894+seoinhyeok96@users.noreply.github.com>
2023-05-03 00:34:36 +0900

    Update ㅠㅠ.py

--- pages/ㅠㅠ.py
+++ pages/ㅠㅠ.py
@@ -25,7 +25,7 @@ st.write('The current movie title is', title)
 
     
 option = st.selectbox(
-    '옵션을 선택하세요',
-    ('자사', '경쟁사')
+    '보고싶은 옵션을 선택하세요!',
+    ('자사', '경쟁사'))
 
-st.write('현재 선택하신 옵션입니다.:',option)
+st.write('현재 선택한 옵션:', option)
