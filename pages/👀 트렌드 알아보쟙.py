41cbacd2d3dc95b2f1dde28153e329e27ce25e7b
hollyisyoon <126076416+hollyisyoon@users.noreply.github.com>
2023-05-03 01:20:28 +0900

    Update 👀 트렌드 알아보쟙.py

--- pages/👀 트렌드 알아보쟙.py
+++ pages/👀 트렌드 알아보쟙.py
@@ -64,6 +64,8 @@ with col3:
     st.write('식물갤러리')   
 
 # Get top words
+df['start_date'] = pd.to_datetime(df['start_date'])
+df['end_date'] = pd.to_datetime(df['end_date'])
 df = df[(df['name'] == '식물갤러리') & (df['time'] >= start_date) & (df['time'] <= end_date)]
 words = get_tfidf_top_words(df, start_date, end_date, keyword_no, '식물갤러리')
 
