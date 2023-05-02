2beea55cdb09c09171368c5377549db1d07bbcaa
Thinking Seo <110218894+seoinhyeok96@users.noreply.github.com>
2023-05-03 01:16:36 +0900

    Update ㅠㅠ.py

--- pages/ㅠㅠ.py
+++ pages/ㅠㅠ.py
@@ -28,15 +28,5 @@ col1, col2 = st.columns(2)
 with col1:
     option_1 = st.selectbox('Option 1', ['자사', '경쟁사'],key='option1')
 with col2:
-        # 시작일과 종료일을 지정합니다.
-    start_date = dt.date(2023, 1, 1)
-    end_date = dt.date.today()
-
-    # 시작일과 종료일 사이의 모든 날짜를 생성합니다.
-    dates = pd.date_range(start_date, end_date)
-
-    # 날짜를 원하는 형식으로 문자열로 변환합니다.
-    date_strings = [date.strftime('%Y-%m-%d') for date in dates]
-
-    # selectbox에 날짜 목록을 전달합니다.
+     # selectbox에 날짜 목록을 전달합니다.
     selected_date = st.selectbox('날짜 선택', date_strings,key='option2')
