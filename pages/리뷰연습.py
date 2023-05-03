f881ea816669b350ed0f25430166bdb5969bdd5f
Song-Juntae <123853844+Song-Juntae@users.noreply.github.com>
2023-05-03 23:08:59 +0900

    .

--- pages/리뷰연습.py
+++ pages/리뷰연습.py
@@ -143,7 +143,7 @@ with elements("dashboard"):
     # as first parameter, plus additional properties you can find in the GitHub links below.
 
     with dashboard.Grid(layout):
-        mui.Box(st.bar_chart(bar_df), key="first_item")
+        mui.Box("dfjkladf", key="first_item")
         mui.Paper("Second item (cannot drag)", key="second_item")
         mui.Paper("Third item (cannot resize)", key="third_item")
 
@@ -203,63 +203,3 @@ with elements("dashboard"):
             )
         mui.Paper("Second item (cannot drag)", key="second_item")
         mui.Paper("Third item (cannot resize)", key="third_item")
-
-with elements("nivo_charts"):
-
-    # Streamlit Elements includes 45 dataviz components powered by Nivo.
-
-    from streamlit_elements import nivo
-
-    DATA = [
-        { "taste": "fruity", "chardonay": 93, "carmenere": 61, "syrah": 114 },
-        { "taste": "bitter", "chardonay": 91, "carmenere": 37, "syrah": 72 },
-        { "taste": "heavy", "chardonay": 56, "carmenere": 95, "syrah": 99 },
-        { "taste": "strong", "chardonay": 64, "carmenere": 90, "syrah": 30 },
-        { "taste": "sunny", "chardonay": 119, "carmenere": 94, "syrah": 103 },
-    ]
-
-    with mui.Box(sx={"height": 500}):
-        nivo.Radar(
-            data=DATA,
-            keys=[ "chardonay", "carmenere", "syrah" ],
-            indexBy="taste",
-            valueFormat=">-.2f",
-            margin={ "top": 70, "right": 80, "bottom": 40, "left": 80 },
-            borderColor={ "from": "color" },
-            gridLabelOffset=36,
-            dotSize=10,
-            dotColor={ "theme": "background" },
-            dotBorderWidth=2,
-            motionConfig="wobbly",
-            legends=[
-                {
-                    "anchor": "top-left",
-                    "direction": "column",
-                    "translateX": -50,
-                    "translateY": -40,
-                    "itemWidth": 80,
-                    "itemHeight": 20,
-                    "itemTextColor": "#999",
-                    "symbolSize": 12,
-                    "symbolShape": "circle",
-                    "effects": [
-                        {
-                            "on": "hover",
-                            "style": {
-                                "itemTextColor": "#000"
-                            }
-                        }
-                    ]
-                }
-            ],
-            theme={
-                "background": "#FFFFFF",
-                "textColor": "#31333F",
-                "tooltip": {
-                    "container": {
-                        "background": "#FFFFFF",
-                        "color": "#31333F",
-                    }
-                }
-            }
-        )
