990570c24188b7e5fcc7f40ea2f7f2f190c96890
hollyisyoon <126076416+hollyisyoon@users.noreply.github.com>
2023-05-03 13:23:25 +0900

    m

--- pages/연습.py
+++ pages/연습.py
@@ -5,4 +5,11 @@ pre_total = pre_input + ["기타"]
 options = st.multiselect(
     'What are your favorite colors',
     pre_total,
-    ['Yellow', 'Red'])
+    ['식물', '화분'])
+
+# import streamlit as st
+# options = st.multiselect(
+#     'What are your favorite colors',
+#     ['Green', 'Yellow', 'Red', 'Blue'],
+#     ['Yellow', 'Red'])
+# st.write('You selected:', options)
