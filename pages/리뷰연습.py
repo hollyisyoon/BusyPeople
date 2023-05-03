26ecd377dc9e6cd259c443c2a8f05fa841407d3d
Song-Juntae <123853844+Song-Juntae@users.noreply.github.com>
2023-05-03 22:29:27 +0900

    .

--- pages/리뷰연습.py
+++ pages/리뷰연습.py
@@ -109,14 +109,15 @@ with st.container():
 
 import streamlit as st
 from streamlit_elements import elements, mui
+from streamlit_elements import dashboard
+
+with dashboard():
+    st.markdown(html, unsafe_allow_html=True)
 
 with elements("dashboard"):
 
     # You can create a draggable and resizable dashboard using
     # any element available in Streamlit Elements.
-
-    from streamlit_elements import dashboard
-
     # First, build a default layout for every element you want to include in your dashboard
 
     layout = [
