3d7e5974cce420f49c929a196566ee19501d8e67
Song-Juntae <123853844+Song-Juntae@users.noreply.github.com>
2023-05-03 11:07:09 +0900

    .

--- pages/디렉토리확인.py
+++ pages/디렉토리확인.py
@@ -9,10 +9,10 @@ file_list = os.listdir(path)
 
 file_list
 
-메모리 = psutil.virtual_memory()
+# 메모리 = psutil.virtual_memory()
 
-메모리
+# 메모리
 
-사용가능한 = 메모리.used / 1024**3
+# 사용가능한 = 메모리.used / 1024**3
 
-사용가능한
+# 사용가능한
