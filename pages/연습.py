3b99a47bd4c1db2a18b52ff6f2e4b8ce0752ba5e
hollyisyoon <126076416+hollyisyoon@users.noreply.github.com>
2023-05-03 13:08:27 +0900

    ,

--- pages/연습.py
+++ pages/연습.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 pre_input = ['식물', '화분', '사진', '오늘']
 options = pre_input + ["기타"]
-stopwords = st.multiselect("옵션을 선택하세요", options, default=["오늘"], allow_input=True)
-st.write(stopwords)
+selected_options = st.multiselect("옵션을 선택하세요", options, default=["오늘"], allow_input=True)
+st.write(selected_options)
