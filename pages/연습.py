191ae46f5045b5d1da974186ec08176fc4a6564c
hollyisyoon <126076416+hollyisyoon@users.noreply.github.com>
2023-05-03 13:14:30 +0900

    d

--- pages/연습.py
+++ pages/연습.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-pre_input = ['식물', '화분', '사진', '오늘']
-options = pre_input + ["기타"]
-selected_options = st.multiselect("옵션을 선택하세요", options, default=["오늘"], allow_input=True)
-st.write(selected_options)
+options = st.multiselect(
+    'What are your favorite colors',
+    ['Green', 'Yellow', 'Red', 'Blue'],
+    ['Yellow', 'Red'])
