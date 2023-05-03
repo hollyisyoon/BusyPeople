b5370448bba66ae35e96baf8aecb64bd8eb98ad5
Song-Juntae <123853844+Song-Juntae@users.noreply.github.com>
2023-05-03 14:26:10 +0900

    .

--- pages/리뷰대시보드.py
+++ pages/리뷰대시보드.py
@@ -9,7 +9,7 @@ from wordcloud import WordCloud
 
 ########################################################################################################################
 
-df_리뷰_감성분석결과 = pd.read_csv('/app/BusyPeople-stramlit/data/리뷰_감성분석결과.csv')
+df_리뷰_감성분석결과 = pd.read_csv('/app/busypeople-stramlit/data/리뷰_감성분석결과.csv')
 df_리뷰_감성분석결과['time'] = pd.to_datetime(df_리뷰_감성분석결과['time'])
 
 파이차트 = px.pie(df['감성결과'].value_counts())
@@ -23,4 +23,3 @@ col1, col2, col3 = st.columns([2,1,1])
 with col1:
    st.plotly_chart(파이차트)
 
-   
