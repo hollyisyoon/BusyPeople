f26eace496570e13c13cb9a073d2d404b4f65172
Song-Juntae <123853844+Song-Juntae@users.noreply.github.com>
2023-05-03 10:20:54 +0900

    .

--- Home.py
+++ Home.py
@@ -18,24 +18,3 @@ path
 file_list = os.listdir(path)
 
 file_list
-
-list_remove = [
-"NanumBarunGothic (2).ttf",
-"NanumBarunGothic (9).ttf",
-"NanumBarunGothic (3).ttf",
-"NanumBarunGothic (13).ttf",
-"NanumBarunGothic (10).ttf",
-"NanumBarunGothic (4).ttf",
-"NanumBarunGothic (15).ttf",
-"NanumBarunGothic (12).ttf",
-"NanumBarunGothic (11).ttf",
-"NanumBarunGothic.ttf",
-"NanumBarunGothic (1).ttf",
-"NanumBarunGothic (8).ttf",
-"NanumBarunGothic (7).ttf",
-"NanumBarunGothic (6).ttf",
-"NanumBarunGothic (14).ttf"
-]
-
-for i in list_remove:
-   os.remove(i)
