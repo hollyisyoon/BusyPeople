8e244d76571d3386134c4e8ef4878059a1b9ab11
min0908 <94737255+min0908@users.noreply.github.com>
2023-05-03 23:19:19 +0900

    .

--- pages/리뷰_토픽연습.py
+++ pages/리뷰_토픽연습.py
@@ -119,47 +119,47 @@ elif dataset == '경쟁사(긍정리뷰)':
 else:
     get_topic_model('/app/busypeople-stramlit/data/경쟁사부정리뷰.csv')
 
+###################################################
+# import openai
+# import streamlit as st
+# from streamlit_chat import message
 
-import openai
-import streamlit as st
-from streamlit_chat import message
-
-# chatGPT
-openai.api_key = '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'
+# # chatGPT
+# openai.api_key = ''
  
-def generate_response(prompt):
-    completions = openai.Completion.create (
-        engine="text-davinci-003",
-        prompt=prompt,
-        max_tokens=1024,
-        stop=None,
-        temperature=0,
-        top_p=1,
-    )
+# def generate_response(prompt):
+#     completions = openai.Completion.create (
+#         engine="text-davinci-003",
+#         prompt=prompt,
+#         max_tokens=1024,
+#         stop=None,
+#         temperature=0,
+#         top_p=1,
+#     )
  
-    message = completions["choices"][0]["text"].replace("\n", "")
-    return message
+#     message = completions["choices"][0]["text"].replace("\n", "")
+#     return message
  
  
-st.markdown("ChatGPT-3 (Demo)")
-st.markdown("궁금한 점은 ChatGPT에게 물어보세요.")
+# st.header("ChatGPT-3 (Demo)")
+# st.markdown("해당 결과를 보고 궁금한 점은 ChatGPT에게 물어보세요.")
  
-if 'generated' not in st.session_state:
-    st.session_state['generated'] = []
+# if 'generated' not in st.session_state:
+#     st.session_state['generated'] = []
  
-if 'past' not in st.session_state:
-    st.session_state['past'] = []
+# if 'past' not in st.session_state:
+#     st.session_state['past'] = []
  
-with st.form('form', clear_on_submit=True):
-    user_input = st.text_input('You: ', '', key='input')
-    submitted = st.form_submit_button('Send')
+# with st.form('form', clear_on_submit=True):
+#     user_input = st.text_input('You: ', '', key='input')
+#     submitted = st.form_submit_button('Send')
  
-if submitted and user_input:
-    output = generate_response(user_input)
-    st.session_state.past.append(user_input)
-    st.session_state.generated.append(output)
+# if submitted and user_input:
+#     output = generate_response(user_input)
+#     st.session_state.past.append(user_input)
+#     st.session_state.generated.append(output)
  
-if st.session_state['generated']:
-    for i in range(len(st.session_state['generated'])-1, -1, -1):
-        message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
-        message(st.session_state["generated"][i], key=str(i))
+# if st.session_state['generated']:
+#     for i in range(len(st.session_state['generated'])-1, -1, -1):
+#         message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
+#         message(st.session_state["generated"][i], key=str(i))
