9ac1252777aeeddcca7793788907c28397d12f0b
hollyisyoon <126076416+hollyisyoon@users.noreply.github.com>
2023-05-03 13:29:31 +0900

    d

--- pages/연습.py
+++ pages/연습.py
@@ -1,10 +1,8 @@
 import streamlit as st
 
-pre_input = ['식물', '화분', '사진', '오늘']
-pre_total = pre_input + ["기타"]
 options = st.multiselect(
     'What are your favorite colors',
-    pre_total,
+    ['식물', '화분', '사진', '오늘'],
     ['식물', '화분'], allow_input=True)
 
 # import streamlit as st
